2e3c39e72264657d5b8ed0dcb4ec8e3dfcf149c6
ZhengKeli <zhengkeli2009@126.com>
2019-08-02 14:59:54 +0800

    improved something in emergency contact

--- app/src/main/java/slw/nightrunning/SettingsActivity.kt
+++ app/src/main/java/slw/nightrunning/SettingsActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_settings.*
 
+
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var settingsPreferences: SharedPreferences
@@ -14,17 +15,30 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         settingsPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE)
+        emergencyContactEnabledCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            emergencyContactNumberField.isEnabled = isChecked
+            emergencyContactMessageField.isEnabled = isChecked
+            if (isChecked) {
+                // todo request permissions
+            }
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onResume() {
+        super.onResume()
+        emergencyContactEnabledCheckBox.isChecked = settingsPreferences.getBoolean("emergencyContactEnabled", false)
         emergencyContactNumberField.setText(settingsPreferences.getString("emergencyContactNumber", ""))
+        val defaultMessage = getString(R.string.default_emergency_contact_message)
+        emergencyContactMessageField.setText(settingsPreferences.getString("emergencyContactMessage", defaultMessage))
     }
 
     override fun onStop() {
         super.onStop()
         val editor = settingsPreferences.edit()
+        editor.putBoolean("emergencyContactEnabled", emergencyContactEnabledCheckBox.isChecked)
         editor.putString("emergencyContactNumber", emergencyContactNumberField.text.toString())
+        editor.putString("emergencyContactMessage", emergencyContactMessageField.text.toString())
         editor.apply()
     }
+
 }
